a238e1f57c18410fc27ad2f85252968e07159aa3
Pramanik1234 <121309897+Pramanik1234@users.noreply.github.com>
2022-12-28 15:28:03 +0530

    Update style.js

--- style.js
+++ style.js
@@ -5,10 +5,11 @@ let link = document.getElementById("link");
 bar.addEventListener('click' , function(){
 link.style.right="0px"
 link.style.display="inline"
+link.style.transition="1s"
 })
 xmark.addEventListener('click' , function(){
 link.style.right="-200px"
-link.style.display="none"
+
 })
 
 
